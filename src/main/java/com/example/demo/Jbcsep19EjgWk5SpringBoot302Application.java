9c06cc3668d198c60f5afd452c2f4a9b4aa8cebb
ericagleaton <ericagleaton@comcast.net>
2019-10-03 14:11:08 -0400

    Data ToolsL Looping Through A List With Thymeleaf

---
+++ src/main/java/com/example/demo/Jbcsep19EjgWk5SpringBoot302Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jbcsep19EjgWk5SpringBoot302Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jbcsep19EjgWk5SpringBoot302Application.class, args);
+    }
+
+}
